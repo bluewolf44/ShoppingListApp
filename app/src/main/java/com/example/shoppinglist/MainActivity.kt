e73700d3356c0f05319797f5707699ced8998f3c
bluewolf44 <danielwest618@gmail.com>
2023-04-16 18:20:27 +1200

    added image and text

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.shoppinglist.ui.theme.ShoppingListTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +25,7 @@ class MainActivity : ComponentActivity() {
             ShoppingListTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ImageTest()
                 }
             }
         }
@@ -27,17 +33,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun ColumnTest(vararg names: String)
+{
+    Column {
+        for(i in names.indices) {
+            val n = names[i]
+            Text(text = "-Hello $n")
+        }
+    }
+}
+
+@Composable
+fun ImageTest()
+{
+    val image = painterResource(id = R.drawable.blue)
+    Box{
+        Image(painter = image, contentDescription = null, modifier = Modifier.fillMaxSize().fillMaxWidth(), contentScale = ContentScale.Crop)
+        ColumnTest("DEs","Daniel","God")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingListTheme {
-        Greeting("Android")
+        ImageTest()
     }
 }
